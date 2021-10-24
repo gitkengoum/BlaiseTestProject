55956e3bb73197b6e77332776cc08e7e01cb6993
Blaise Kengoum <kengoum@hotmail.com>
2021-10-24 07:10:02 -0400

    Initial Commit

---
+++ BlaiseTestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlaiseTestProject
+//
+//  Created by BLAISE KENGOUM on 10/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
